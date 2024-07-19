68c71ac700015b8cecca7cdc73be72ec8e6f0609
Stu <stumac1000@yahoo.com>
2024-07-19 12:19:20 -0700

    Finish lesson 1.5
    Save Todo List to local storage utilizing the
    useEffect Hook.
    
    Create a custom hook.
    
    Utilize React fragments in our app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("savedTodoList")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+};
+
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => setTodoList([...todoList, newTodo]);
 
